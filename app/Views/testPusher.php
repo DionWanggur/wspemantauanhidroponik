ace26fa5b64474e9c153968e269122666d05081d
rockStar14 <48817304+DionWanggur@users.noreply.github.com>
2022-06-06 02:38:14 +0700

    Update testPusher.php
    update Dashboard request

--- app/Views/testPusher.php
+++ app/Views/testPusher.php
@@ -18,6 +18,7 @@
         channel.bind('my-event', function(data) {
             //alert(JSON.stringify(data));
             loadDoc();
+            loadDoc2();
         });
 
         function loadDoc() {
@@ -27,18 +28,39 @@
                 document.getElementById("namaHidroponik").innerHTML = JSONarray[0].namaHidroponik
                 document.getElementById("lokasi").innerHTML = JSONarray[0].lokasi
                 document.getElementById("namaNode").innerHTML = JSONarray[0].namaNode;
-                document.getElementById("waktu").innerHTML = JSONarray[0].waktu;
+                document.getElementById("waktu").innerHTML = Date.parse(JSONarray[0].waktu);
                 var content = "";
 
                 for (let i in JSONarray) {
-                    
+
                     content += JSONarray[i].namaSensor + " : " + JSONarray[i].value + "\n";
-                    console.log(content);
                 }
 
                 document.getElementById("demo").innerHTML = content;
             }
             xhttp.open("GET", "https://wspemantauanhidroponik.herokuapp.com/monitoring?namaNode=Node%20Dua", true);
+            xhttp.responseText = "json";
+            xhttp.send();
+        }
+
+        function loadDoc2() {
+            const xhttp = new XMLHttpRequest();
+            xhttp.onload = function() {
+                var JSONarray = JSON.parse(this.responseText);
+                document.getElementById("namaHidroponik1").innerHTML = JSONarray[0].namaHidroponik
+                document.getElementById("lokasi1").innerHTML = JSONarray[0].lokasi
+                document.getElementById("namaNode1").innerHTML = JSONarray[0].namaNode;
+                document.getElementById("waktu1").innerHTML = Date.parse(JSONarray[0].waktu);
+                var content = "";
+
+                for (let i in JSONarray) {
+
+                    content += JSONarray[i].namaSensor + " : " + JSONarray[i].value + "\n";
+                }
+
+                document.getElementById("demo1").innerHTML = content;
+            }
+            xhttp.open("GET", "https://wspemantauanhidroponik.herokuapp.com/monitoring?namaNode=Node%20Satu", true);
             xhttp.responseText = "json";
             xhttp.send();
         }
@@ -52,15 +74,28 @@
             Try publishing an event to channel <code>my-channel</code>
             with event name <code>my-event</code>.
         </p>
+        <div class="container">
+            <h5 id="namaHidroponik"></h5>
+            <h5 id="lokasi"></h5>
+            <h5 id="namaNode"></h5>
+            <h5>Waktu Pemantauan: </h5>
+            <h5 id="waktu">Waktu Pemantauan: </h5>
 
-        <h5 id="namaHidroponik"></h5>
-        <h5 id="lokasi"></h5>
-        <h5 id="namaNode"></h5>
-        <h5>Waktu Pemantauan: </h5>
-        <h5 id="waktu">Waktu Pemantauan: </h5>
-        
 
-        <p id="demo"></p>
+            <p id="demo"></p>
+        </div>
+
+        <div class="container">
+            <h5 id="namaHidroponik1"></h5>
+            <h5 id="lokasi1"></h5>
+            <h5 id="namaNode1"></h5>
+            <h5>Waktu Pemantauan: </h5>
+            <h5 id="waktu1">Waktu Pemantauan: </h5>
+
+
+            <p id="demo1"></p>
+        </div>
+
 
     </div>
 
